32d8eb623f546b6cb918abd8622c9e58751f6226
Dylan Scott <dylan@dylanscott.com.au>
2015-01-26 15:44:37 +1100

    Started adding views.

--- weblog/__init__.py
+++ weblog/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
+app.config.from_pyfile('weblog.cfg')
 
-import weblog.views
+from weblog.views import weblog
+
+app.register_blueprint(weblog, url_prefix='', static_folder='static', static_url_path='static')
--- weblog/views.py
+++ weblog/views.py
@@ -1,5 +1,19 @@
-from weblog import app
+from flask import Blueprint, render_template
 
-@app.route('/')
+weblog = Blueprint('weblog', __name__, static_folder='static')
+
+@weblog.route('/')
 def index():
-    return 'hello world'
+    return render_template('weblog.html')
+
+@weblog.route('/projects')
+def projects():
+    return render_template('projects.html', title='Projects')
+
+@weblog.route('/notes')
+def notes():
+    return render_template('notes.html', title='Notes')
+
+@weblog.route('/contact')
+def contact():
+    return render_template('contact.html', title='Contact')
