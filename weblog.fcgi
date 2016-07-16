5b7e755917cdc35c7f27cff786a12c2cab051844
Dylan Scott <dylan@dylanscott.com.au>
2016-07-17 09:25:06 +1000

    Delete everything

--- weblog.fcgi
+++
@@ -1,6 +0,0 @@
-#!/usr/local/bin/python2.7
-from flup.server.fcgi import WSGIServer
-from weblog import app
-
-if __name__ == '__main__':
-    WSGIServer(app, bindAddress='/var/www/run/weblog.sock').run()
--- weblog/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-app.config.from_pyfile('weblog.cfg')
-
-from weblog.views import weblog
-
-app.register_blueprint(weblog, url_prefix='', static_folder='static', static_url_path='static')
